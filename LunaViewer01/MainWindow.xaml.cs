9f0d517a1d056845898a74185a5d1d18b07a412a
huonchan <dmht1127@gmail.com>
2021-01-06 14:06:31 +0900

    拡縮無理やり対応

--- LunaViewer01/MainWindow.xaml.cs
+++ LunaViewer01/MainWindow.xaml.cs
@@ -119,10 +119,38 @@ namespace LunaViewer01
                     inSource.EndInit();
                     MainImage.Source = inSource;
                     MainImage.Width = inSource.PixelWidth;// * 2;
-                    MainImage.Height = inSource.PixelHeight;// * 2;
+                    MainImage.Height = inSource.PixelHeight;// * 2;*
 
-                    label_WidthNum.Content = inSource.PixelWidth;
+                    //label_WidthNum.Content = inSource.PixelWidth;
+                    label_WidthNum.Content = rate.Value;
                     label_HeightNum.Content = inSource.PixelHeight;
+
+                    // canvasの拡大縮小
+                    //rate.Value = 0;
+
+                    rate.Value = 100;
+                    //  マウスホイールのイベントを受け取り、スライダーをずらす
+                    var index = rate.Ticks.IndexOf(rate.Value);
+                    Double scale = rate.Ticks[index] / rate.Value;
+                    rate.Value = rate.Ticks[index];
+
+                    // canvasサイズの変更
+                    canvas.Height *= scale;
+                    canvas.Width *= scale;
+
+                    //MainImage.Width = inSource.PixelWidth * scale;
+                    //MainImage.Height = inSource.PixelHeight * scale;
+
+                    // canvasの拡大縮小
+                    Matrix m0 = new Matrix();
+                    m0.Scale(rate.Value * 0.01, rate.Value * 0.01);//元のサイズとの比
+                                                                   //m0.Transform(new Point(500, 500));
+                                                                   //m0.Translate(200, 300);
+                    matrixTransform.Matrix = m0;
+
+
+                    scrollViewer.CaptureMouse();
+
 
 
                     ToggleButton cButton = s as ToggleButton;
@@ -136,10 +164,10 @@ namespace LunaViewer01
                     cButton.IsChecked = true;
                     currentButton = cButton;
 
-                    zoomValue = 1.0f;
+                   // zoomValue = 1.0f;
 
-                    ScaleTransform scale = new ScaleTransform(zoomValue, zoomValue);
-                    MainImage.LayoutTransform = scale;
+                    //ScaleTransform scale2= new ScaleTransform(zoomValue, zoomValue);
+                    //MainImage.LayoutTransform = scale2;
 
                     //MessageBox.Show(fles);
 
@@ -152,39 +180,95 @@ namespace LunaViewer01
 
         }
 
-        //https://www.c-sharpcorner.com/forums/scroll-a-view-horizontally-using-the-mouse-wheel-in-wpf
-        //todo:https://stackoverflow.com/questions/3727439/how-to-enable-horizontal-scrolling-with-mouse が使えるか確かめる
-        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        /// <summary>
+        /// 画像変更時のCanvasサイズの変更
+        /// Scrollviewerのスクロールを出す用
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void image_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            //if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+            canvas.Height = e.NewSize.Height;
+            canvas.Width = e.NewSize.Width;
+        }
+
+        private void canvas_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+
+            if ((Keyboard.Modifiers & ModifierKeys.Control) <= 0)
             {
-                if (e.Delta > 0)
-                    dummyScroll.PageLeft();
-                else
-                    dummyScroll.PageRight();
-                e.Handled = true;
+                return;
+            }
+
+            // 後続のイベントを実行しないための処理
+            e.Handled = true;
+
+            //  マウスホイールのイベントを受け取り、スライダーをずらす
+            var index = rate.Ticks.IndexOf(rate.Value);
+            if (0 < e.Delta)
+            {
+                index += 1;
+            }
+            else
+            {
+                index -= 1;
+            }
+
+            if (index < 0)
+            {
+                return;
+            }
+            else if (rate.Ticks.Count <= index)
+            {
+                return;
+            }
+
+            Double scale = rate.Ticks[index] / rate.Value;
+            rate.Value = rate.Ticks[index];
+
+            // canvasサイズの変更
+            canvas.Height *= scale;
+            canvas.Width *= scale;
+
+            // canvasの拡大縮小
+            Matrix m0 = new Matrix();
+            m0.Scale(rate.Value * 0.01, rate.Value * 0.01);//元のサイズとの比
+            //m0.Transform(new Point(500, 500));
+            //m0.Translate(200, 300);
+            matrixTransform.Matrix = m0;
+
+            // scrollViewerのスクロールバーの位置をマウス位置を中心とする。
+            Point mousePoint = e.GetPosition(scrollViewer);
+            Double x_barOffset = (scrollViewer.HorizontalOffset + mousePoint.X) * scale - mousePoint.X;
+            scrollViewer.ScrollToHorizontalOffset(x_barOffset);
+
+            Double y_barOffset = (scrollViewer.VerticalOffset + mousePoint.Y) * scale - mousePoint.Y;
+            scrollViewer.ScrollToVerticalOffset(y_barOffset);
+        }
+
+        Point scrollMousePoint = new Point();
+        double hOff = 1;
+        double vOff = 1;
+        private void scrollViewer_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            scrollMousePoint = e.GetPosition(scrollViewer);
+            hOff = scrollViewer.HorizontalOffset;
+            vOff = scrollViewer.VerticalOffset;
+            scrollViewer.CaptureMouse();
+        }
+
+        private void scrollViewer_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (scrollViewer.IsMouseCaptured)
+            {
+                scrollViewer.ScrollToHorizontalOffset(hOff + (scrollMousePoint.X - e.GetPosition(scrollViewer).X));
+                scrollViewer.ScrollToVerticalOffset(vOff + (scrollMousePoint.Y - e.GetPosition(scrollViewer).Y));
             }
         }
 
-        private void scrollviewer_image_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        private void scrollViewer_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            //if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
-            {
-                if (e.Delta > 0)
-                {
-
-                    zoomValue += 0.1;
-                }
-
-                else
-                {
-                    zoomValue -= 0.1;
-                }
-
-            }
-
-            ScaleTransform scale = new ScaleTransform(zoomValue, zoomValue);
-            MainImage.LayoutTransform = scale;
+            scrollViewer.ReleaseMouseCapture();
         }
 
     }
